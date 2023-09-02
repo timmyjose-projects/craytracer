debdbfaf7549aca96ded63fa1d2f96f18de58b34
Timmy Jose <zoltan.jose@gmail.com>
2023-09-02 11:20:48 +0530

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "roots": [
+    "<rootDir>"
+  ],
+  "testMatch": [
+    "<rootDir>/tests/**/*.*(ts|tsx|js|)"
+  ],
+  "transform": {
+    "^.*\\.(ts|tsx)$": "ts-jest"
+  }
+}
